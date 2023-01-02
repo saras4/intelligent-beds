6bc7c8395ec3feba24c9178b86bf0fe45cb920e6
saras <dimitrissaras4@gmail.com>
2023-01-02 14:49:25 +0200

    Add header

--- patient-status.php
+++ patient-status.php
@@ -1,5 +1,8 @@
 <?php include "db.php";?>
 <?php include "intelligent-funtions.php";?>
 <?php include "header.php";?>
-
+  <div class="patient">
+    <div class="patient-info"></div>
+    <div class="patient-current-stage"></div>
+  </div>
 <?php include "footer.php";?>
