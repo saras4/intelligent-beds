f3120f2af5d9949435eb5a7cfa25a125f02de217
saras <dimitrissaras4@gmail.com>
2023-01-03 21:49:15 +0200

    Add search function

--- patient-status.php
+++ patient-status.php
@@ -1,10 +1,15 @@
 <?php include "db.php";?>
 <?php include "intelligent-funtions.php";?>
 <?php include "header.php";?>
-  <!-- <div class="patient">
-    <div class="patient-info"></div>
-    <div class="patient-current-stage"></div>
-  </div> -->
+<div class="patient">
+  <div class="patient-info">
+  <!-- <?php readPatients();?> -->
 
-  <?php amkaPost();?>
+  </div>
+  <div class="patient-current-stage">
+
+  </div>
+  </div>
+
+  <?php search();?>
 <?php include "footer.php";?>
