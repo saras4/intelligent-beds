d2ce4078dfe7adc74213491232a68bc4e958eba3
saras <dimitrissaras4@gmail.com>
2023-03-08 23:50:27 +0200

    change name for landing page

--- landing-page.php
+++ landing-page.php
@@ -1,5 +1,5 @@
 <?php include "header.php";?>
 <div class="landing-middle">
-  <div class="big-font">This is a landing page to manage patients</div>
+  <div class="big-font">This is THE landing page to manage patients</div>
 </div>
 <?php include "footer.php";?>
