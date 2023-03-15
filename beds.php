1e17ae88e484da45ae58ed821dbabf885023277a
saras <dimitrissaras4@gmail.com>
2023-03-15 15:30:17 +0200

    new pages to show tables

---
+++ beds.php
@@ -0,0 +1,5 @@
+<?php include "header.php";?>
+<div class="landing-middle">
+  <div class="big-font">Beds CRUD</div>
+</div>
+<?php include "footer.php";?>
---
+++ patients.php
@@ -0,0 +1,5 @@
+<?php include "header.php";?>
+<div class="landing-middle">
+  <div class="big-font">Patients Crud</div>
+</div>
+<?php include "footer.php";?>
