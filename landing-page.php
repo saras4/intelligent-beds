9b1df7f2e06b349ceb159d2e77e15702ab521340
saras <dimitrissaras4@gmail.com>
2023-03-27 19:22:03 +0300

    Add landing options and style

--- landing-page.php
+++ landing-page.php
@@ -1,5 +1,20 @@
 <?php include "header.php";?>
 <div class="landing-middle">
-  <div class="big-font">This is a monitoring system for bedridden patients.Please choose one of the options</div>
+  <div class="big-font">This is a monitoring system for bedridden patients.Please choose one of the options:</div>
+</div>
+<div class="middle-options">
+  <div class="options">  
+    <img src="icons/medical.png" alt="manage patients"> 
+    <h3>Manage Patients</h3>
+  </div>
+  <div class="options">  
+    <img src="icons/hospital-bed.png" alt="manage beds"> 
+    <h3>Manage beds</h3>
+  </div>
+  <div class="options">  
+    <img src="icons/patient.png" alt="monitor patients"> 
+    <h3>monitor patients</h3>
+  </div>
+
 </div>
 <?php include "footer.php";?>
