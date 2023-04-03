6cdbe8fb0515301028e398e4c7d7798e5045da12
saras <dimitrissaras4@gmail.com>
2023-04-03 18:50:34 +0300

    Fix navbar options

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<?php include "header.php";?>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Inspect</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+  <div class="header">
+    <div class="logo"> <a href="index.php">Inspect System </a></div>
+    <!-- <div class="header-links">
+      <ul>
+        <li><a href="beds.php">Manage Beds</a></li>
+        <li><a href="patients.php">Manage Patients</a></li>
+      </ul>
+    </div> -->
+  </div>
 <div class="landing-middle">
   <div class="big-font">This is a monitoring system for bedridden patients.Please choose one of the options:</div>
 </div>
@@ -21,7 +43,7 @@
   <a href="monitor-patients.php">
   <div class="options">  
     <img src="icons/patient.png" alt="monitor patients"> 
-    <h3>monitor patients</h3>
+    <h3>Monitor patients</h3>
   </div>
   </a>
 
--- patients.php
+++ patients.php
@@ -1,5 +1,27 @@
-<?php include "header.php";?>
-<div class="landing-middle">
-  <div class="big-font">Patients Crud</div>
-</div>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Inspect</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+  <div class="header">
+    <div class="logo"> <a href="index.php">Inspect System </a></div>
+    <div class="header-links">
+      <ul>
+        <li><a href="beds.php">Manage Beds</a></li>
+        <li><a href="monitor-patients.php">Monitor Patients</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="landing-middle">
+    <div class="big-font">Patients Crud</div>
+  </div>
 <?php include "footer.php";?>
